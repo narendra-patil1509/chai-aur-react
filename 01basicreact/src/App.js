2f4083f8332578eb8b6db77b48a81381cd975317
narendra-patil1509 <np738587@gmail.com>
2024-03-17 21:27:12 +0530

    regular js works

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,11 +1,16 @@
-
+import Chai from "./Chai";
 
 function App() {
   return (
-    <h1>Chai aur React
-      <br />
-      note : In this project I learned how to make basic react app using js and how to clean code or files for easy understanding
-    </h1>
+    <>
+      <h1>
+        Chai aur React
+        <br />
+        note : In this project I learned how to make basic react app using js
+        and how to clean code or files for easy understanding
+      </h1>
+      <Chai />
+    </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <h1>Chai in react
+        <br />
+        In this project I learned about naming convention like component name should be start from capital letter, export import with capital letter  and regular js works
+         </h1>
+    );
+}
+
+export default Chai; 
