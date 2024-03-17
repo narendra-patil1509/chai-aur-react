acfceb17eab435dc4977fd7070cca03412f89d8e
narendra-patil1509 <np738587@gmail.com>
2024-03-17 19:07:46 +0530

    basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <h1>Chai aur React
+      <br />
+      note : In this project I learned how to make basic react app using js and how to clean code or files for easy understanding
+    </h1>
+  );
+}
+
+export default App;
