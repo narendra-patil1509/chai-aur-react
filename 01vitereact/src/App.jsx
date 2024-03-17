002b4e155b981b4e3d88a71993ee017a2b910224
narendra-patil1509 <np738587@gmail.com>
2024-03-17 19:08:14 +0530

    basic react app using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur react with vite | HC 
+      <br />
+      note : In this project I learned how to make react app using vite and how to clean code or files for easy understanding 
+    </h1>
+  )
+}
+
+export default App
