688b94fd614240d394c924d68b264b03bc8f3602
narendra-patil1509 <np738587@gmail.com>
2024-03-18 16:43:10 +0530

    How React read components works

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type:'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit'
+// }
+const anotherUser = 'This is Variable';
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+//React read every component as like tree structure below reactElement is example
+const reactElement = React.createElement(
+  'a',
+  {href:'https://netflix.com',target:'_blank'},
+  'Netflix Go',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
+  // <App/>
 )
