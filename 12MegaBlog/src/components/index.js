b87a6653be7d278aa39273a6e0db5a169953b4fb
narendra-patil1509 <np738587@gmail.com>
2024-11-14 18:10:50 +0530

    updated exports

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -6,8 +6,11 @@ import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup"
 import Login from "./Login"
+import Button from "./Button"
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./Authlayout";
+import Input from "./Input";
+import Select from "./Select";
 
-export {Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, PostForm, PostCard, AuthLayout};
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, Button, PostForm, PostCard, AuthLayout, Input, Select};
